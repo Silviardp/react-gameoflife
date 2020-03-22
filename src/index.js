bbd2f22d37c66e56e02f52907ff518f3d103a270
Silvia Rocha <silviadprocha@gmail.com>
2020-03-22 15:49:16 +0900

    main component + css

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Grid extends React.Component {
+  render() {
+    return <div>Grid</div>;
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Main extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      generation: 0
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1>The Game of life</h1>
+        <Grid />
+        <h2>Generations:{this.state.generation}</h2>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Main />, document.getElementById("root"));
